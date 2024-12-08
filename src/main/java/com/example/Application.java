eee98212367f725b6813fee7bcae565019174baa
yangfeifei <2998433993@qq.com>
2024-12-08 17:01:04 +0800

    first commit

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Starting Spring Boot Application...");
         SpringApplication.run(Application.class, args);
-        System.out.println("Spring Boot Application Started.");
     }
 }
